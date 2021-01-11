34baff507a1c761a17641a5aab39b2e11f69f1c5
Михаил Скляров <sklyarov.m@roistat.com>
2021-01-11 22:20:29 +0300

    Decrease interval between prices refreshment

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new Jobs\RefreshMarketOrders())->everyTwoHours();
+        $schedule->job(new Jobs\RefreshMarketOrders())->everyFifteenMinutes();
     }
 
     /**
