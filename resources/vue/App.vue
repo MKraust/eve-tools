b2c50dfbe0634f891fe5b27c3ca13a3e74dba2d1
Михаил Скляров <sklyarov.m@roistat.com>
2021-05-12 20:58:19 +0300

    Refactor metronic assets

--- resources/vue/App.vue
+++ resources/vue/App.vue
@@ -13,15 +13,6 @@ export default {
 <style lang="scss">
     // 3rd party plugins css
     @import "~bootstrap-vue/dist/bootstrap-vue.css";
-    @import "~perfect-scrollbar/css/perfect-scrollbar.css";
-    @import "~socicon/css/socicon.css";
-    @import "~@fortawesome/fontawesome-free/css/all.css";
-    @import "~line-awesome/dist/line-awesome/css/line-awesome.css";
-    @import "~sweetalert2/src/sweetalert2.scss";
-    @import "metronic/plugins/flaticon/flaticon.css";
-    @import "metronic/plugins/flaticon2/flaticon.css";
-    @import "metronic/plugins/keenthemes-icons/font/ki.css";
-    @import "metronic/sass/style.scss";
 
     .monospace {
       font-family: Consolas,Monaco, "Andale Mono", "Ubuntu Mono", monospace;
--- resources/vue/metronic/index.js
+++ resources/vue/metronic/index.js
@@ -1,3 +1,5 @@
+import './sass/style.scss';
+
 //
 // 3rd-Party Plugins JavaScript Includes
 //
@@ -40,49 +42,49 @@ require('block-ui/jquery.blockUI.js');
 
 // Sweetalert2 - a beautiful, responsive, customizable and accessible (WAI-ARIA) replacement for JavaScript's popup boxes: https://sweetalert2.github.io/
 window.Swal = window.swal = require('sweetalert2/dist/sweetalert2.min.js');
-require('@/metronic/js/vendors/plugins/sweetalert2.init.js');
+require('./js/vendors/plugins/sweetalert2.init.js');
 
 // Keenthemes' plugins
-window.KTUtil = require('@/metronic/js/components/util.js');
-window.KTApp = require('@/metronic/js/components/app.js');
-window.KTCard = require('@/metronic/js/components/card.js');
-window.KTCookie = require('@/metronic/js/components/cookie.js');
-window.KTDialog = require('@/metronic/js/components/dialog.js');
-window.KTHeader = require('@/metronic/js/components/header.js');
-window.KTImageInput = require('@/metronic/js/components/image-input.js');
-window.KTMenu = require('@/metronic/js/components/menu.js');
-window.KTOffcanvas = require('@/metronic/js/components/offcanvas.js');
-window.KTScrolltop = require('@/metronic/js/components/scrolltop.js');
-window.KTToggle = require('@/metronic/js/components/toggle.js');
-window.KTWizard = require('@/metronic/js/components/wizard.js');
-require('@/metronic/js/components/datatable/core.datatable.js');
-require('@/metronic/js/components/datatable/datatable.checkbox.js');
+window.KTUtil = require('./js/components/util.js');
+window.KTApp = require('./js/components/app.js');
+window.KTCard = require('./js/components/card.js');
+window.KTCookie = require('./js/components/cookie.js');
+window.KTDialog = require('./js/components/dialog.js');
+window.KTHeader = require('./js/components/header.js');
+window.KTImageInput = require('./js/components/image-input.js');
+window.KTMenu = require('./js/components/menu.js');
+window.KTOffcanvas = require('./js/components/offcanvas.js');
+window.KTScrolltop = require('./js/components/scrolltop.js');
+window.KTToggle = require('./js/components/toggle.js');
+window.KTWizard = require('./js/components/wizard.js');
+require('./js/components/datatable/core.datatable.js');
+require('./js/components/datatable/datatable.checkbox.js');
 
 // Metronic layout base js
-window.KTLayoutAside = require('@/metronic/js/layout/base/aside.js');
-window.KTLayoutAsideMenu = require('@/metronic/js/layout/base/aside-menu.js');
-window.KTLayoutAsideToggle = require('@/metronic/js/layout/base/aside-toggle.js');
-window.KTLayoutBrand = require('@/metronic/js/layout/base/brand.js');
-window.KTLayoutContent = require('@/metronic/js/layout/base/content.js');
-window.KTLayoutFooter = require('@/metronic/js/layout/base/footer.js');
-window.KTLayoutHeader = require('@/metronic/js/layout/base/header.js');
-window.KTLayoutHeaderMenu = require('@/metronic/js/layout/base/header-menu.js');
-window.KTLayoutHeaderTopbar = require('@/metronic/js/layout/base/header-topbar.js');
-window.KTLayoutStickyCard = require('@/metronic/js/layout/base/sticky-card.js');
-window.KTLayoutStretchedCard = require('@/metronic/js/layout/base/stretched-card.js');
-window.KTLayoutSubheader = require('@/metronic/js/layout/base/subheader.js');
+window.KTLayoutAside = require('./js/layout/base/aside.js');
+window.KTLayoutAsideMenu = require('./js/layout/base/aside-menu.js');
+window.KTLayoutAsideToggle = require('./js/layout/base/aside-toggle.js');
+window.KTLayoutBrand = require('./js/layout/base/brand.js');
+window.KTLayoutContent = require('./js/layout/base/content.js');
+window.KTLayoutFooter = require('./js/layout/base/footer.js');
+window.KTLayoutHeader = require('./js/layout/base/header.js');
+window.KTLayoutHeaderMenu = require('./js/layout/base/header-menu.js');
+window.KTLayoutHeaderTopbar = require('./js/layout/base/header-topbar.js');
+window.KTLayoutStickyCard = require('./js/layout/base/sticky-card.js');
+window.KTLayoutStretchedCard = require('./js/layout/base/stretched-card.js');
+window.KTLayoutSubheader = require('./js/layout/base/subheader.js');
 
 // Metronic layout extended js
-window.KTLayoutChat = require('@/metronic/js/layout/extended/chat.js');
-window.KTLayoutDemoPanel = require('@/metronic/js/layout/extended/demo-panel.js');
-window.KTLayoutExamples = require('@/metronic/js/layout/extended/examples.js');
-window.KTLayoutQuickActions = require('@/metronic/js/layout/extended/quick-actions.js');
-window.KTLayoutQuickCartPanel = require('@/metronic/js/layout/extended/quick-cart.js');
-window.KTLayoutQuickNotifications = require('@/metronic/js/layout/extended/quick-notifications.js');
-window.KTLayoutQuickPanel = require('@/metronic/js/layout/extended/quick-panel.js');
-window.KTLayoutQuickSearch = require('@/metronic/js/layout/extended/quick-search.js');
-window.KTLayoutQuickUser = require('@/metronic/js/layout/extended/quick-user.js');
-window.KTLayoutScrolltop = require('@/metronic/js/layout/extended/scrolltop.js');
-window.KTLayoutSearch = window.KTLayoutSearchOffcanvas = KTLayoutSearchInline = require('@/metronic/js/layout/extended/search.js');
+window.KTLayoutChat = require('./js/layout/extended/chat.js');
+window.KTLayoutDemoPanel = require('./js/layout/extended/demo-panel.js');
+window.KTLayoutExamples = require('./js/layout/extended/examples.js');
+window.KTLayoutQuickActions = require('./js/layout/extended/quick-actions.js');
+window.KTLayoutQuickCartPanel = require('./js/layout/extended/quick-cart.js');
+window.KTLayoutQuickNotifications = require('./js/layout/extended/quick-notifications.js');
+window.KTLayoutQuickPanel = require('./js/layout/extended/quick-panel.js');
+window.KTLayoutQuickSearch = require('./js/layout/extended/quick-search.js');
+window.KTLayoutQuickUser = require('./js/layout/extended/quick-user.js');
+window.KTLayoutScrolltop = require('./js/layout/extended/scrolltop.js');
+window.KTLayoutSearch = window.KTLayoutSearchOffcanvas = require('./js/layout/extended/search.js');
 
-require('@/metronic/js/layout/initialize.js');
+require('./js/layout/initialize.js');
